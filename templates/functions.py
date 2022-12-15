4601ce5e2fc535d3a64e39202a3187c1ff13dea1
Douglas Cardoso <douglas.dcm@gmail.com>
2022-12-14 23:40:01 -0300

    imrove-dockerfile

--- templates/functions.py
+++ templates/functions.py
@@ -3,78 +3,78 @@ from src.courses import Courses
 from src.disciplines import Disciplines
 from src.config import conn
 from src.utils.utils import inicializa_tabelas
-from pyscript import Element
+from datetime import datetime
+from pyscript import Element, create
 
 inicializa_tabelas(conn)
 
 
-def inscreve_materia():
+def subscribe_discipline():
     try:
-        aluno_id = Element("inscreve-aluno-id")
-        materia_id = Element("inscreve-materia-id")
+        aluno_id = Element("subscribe-student-id")
+        materia_id = Element("subscribe-discipline-id")
         students = Students(conn)
         students.inscreve_materia(aluno_id.value, materia_id.value)
         qtde = len(students.lista_tudo())
-        print(
-            "#Aluno",
-            qtde,
-            " Nome ",
-            students.lista(qtde).nome,
-            " Materia ",
-            Disciplines(conn).lista(qtde).materia_id,
-        )
+        text = f"#Student id {qtde}, Name {students.lista(qtde).nome}, Discipline id {Disciplines(conn).lista(qtde).materia_id}"
+        __update_terminal(text, "INFO")
     except Exception as e:
-        print(e)
+        __update_terminal(e, "FAIL")
 
 
-def add_materia():
+def add_discipline():
     try:
-        materia_nome = Element("materia-nome")
-        curso_materia_id = Element("curso-materia-id")
+        discipline_nome = Element("discipline-nome")
+        curso_discipline_id = Element("course-discipline-id")
         disciplines = Disciplines(conn)
-        disciplines.cria(materia_nome.value, curso_materia_id.value)
+        disciplines.cria(discipline_nome.value, curso_discipline_id.value)
         qtde = len(disciplines.lista_tudo())
-        print(
-            "#Materia",
-            qtde,
-            " Nome ",
-            disciplines.lista(qtde).nome,
-            " Curso ",
-            disciplines.lista(qtde).curso_id,
-        )
+        text = f"#Added discipline id: {qtde}, Name: {disciplines.lista(qtde).nome}, Course: {disciplines.lista(qtde).curso_id}"
+        __update_terminal(text, "INFO")
     except Exception as e:
-        print(e)
+        __update_terminal(e, "FAIL")
 
 
-def inscreve_curso():
+def subscribe_course():
     try:
-        aluno_id = Element("aluno-id")
-        curso_id = Element("curso-id")
+        aluno_id = Element("student-id")
+        curso_id = Element("course-id")
         students = Students(conn)
         students.inscreve_curso(aluno_id.value, curso_id.value)
         qtde = len(students.lista_tudo())
-        print("#Aluno", qtde, " Curso ", students.lista(qtde).curso_id)
+        text = f"#Student id {qtde} subscribed to course id {students.lista(qtde).curso_id}"
+        __update_terminal(text, "INFO")
     except Exception as e:
-        print(e)
+        __update_terminal(e, "FAIL")
 
 
-def add_curso():
+def add_course():
     try:
-        content = Element("curso-nome")
+        content = Element("course-nome")
         courses = Courses(conn)
         courses.cria(content.value)
         qtde = len(courses.lista_tudo())
-        print("#Curso", qtde, " Nome ", courses.lista(qtde).nome)
+        text = f"#Course id: {qtde}, Nome: {courses.lista(qtde).nome}"
+        __update_terminal(text, "INFO")
     except Exception as e:
-        print(e)
+        __update_terminal(e, "FAIL")
 
 
-def add_aluno():
+def add_student():
     try:
-        content = Element("aluno-nome")
+        content = Element("student-nome")
         students = Students(conn)
         students.cria(content.value)
         qtde = len(students.lista_tudo())
-        print("#Aluno", qtde, " Nome ", students.lista(qtde).nome)
+        text = f"Added student. id: {qtde}, Name: {students.lista(qtde).nome}"
+        __update_terminal(text, "INFO")
     except Exception as e:
-        print(e)
+        __update_terminal(e, "FAIL")
+
+
+def __update_terminal(text, message_type):
+    terminal = Element("local-terminal")
+    item = create("pre", classes="py-p")
+    item.element.innerText = f"{datetime.now()} {message_type} {text}"
+    terminal.element.appendChild(item.element)
+    terminal.element.insertBefore(item.element, terminal.element.childNodes[0])
--- utils/start.sh
+++ utils/start.sh
@@ -1,4 +1,4 @@
-BASE_DIR="/"
+BASE_DIR="/webapp"
 # start the server
 # python ${BASE_DIR}/cli.py --initdb
 python ${BASE_DIR}/app.py
