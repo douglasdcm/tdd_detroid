a813ea9dbd4233a57cf14fb9a375d00a9e88f069
Douglas Cardoso <douglas.dcm@gmail.com>
2023-02-10 01:51:31 -0300

    All tests passing after revert and cherry-pick code

--- src/controllers/student.py
+++ src/controllers/student.py
@@ -16,6 +16,7 @@ class StudentController:
         self._conn = conn
         self._aluno_id = None
         self._student_bl = StudentBL()
+        self._storage = StudentStorage(conn)
 
     @property
     def id(self):
@@ -24,14 +25,14 @@ class StudentController:
     @id.setter
     def id(self, valor):
         self._aluno_id = valor
-        self._student_storage.get_student(self._aluno_id)
+        self._storage.get_student(self._aluno_id)
 
     def set_grade(self, discipline_id, grade):
         grade = int(grade)
         self._student_bl.check_grade_boundaries(grade)
-        self._student_storage.check_student_in_discipline(self._aluno_id, discipline_id)
-        self._student_storage.update_grade(self._aluno_id, discipline_id, grade)
-        self._student_storage.calculate_coef_rend(self._aluno_id)
+        self._storage.check_student_in_discipline(self._aluno_id, discipline_id)
+        self._storage.update_grade(self._aluno_id, discipline_id, grade)
+        self._storage.calculate_coef_rend(self._aluno_id)
 
     def create(self, nome):
         nome = self._student_bl.clear_name(nome)
@@ -39,17 +40,15 @@ class StudentController:
         self._aluno_id = self._storage.get_maximum_id()
 
     def subscribe_in_discipline(self, materia_id):
-        curso_id = self._student_storage.get_course_id(self._aluno_id)
+        curso_id = self._storage.get_course_id(self._aluno_id)
         DisciplineController(self._conn).check_exists(materia_id, curso_id)
-        self._student_storage.create(nome)
-        self._aluno_id = self._student_storage.get_maximum_id()
-        self._student_storage.check_student_already_in_discipline(
-            self._aluno_id, materia_id
-        )
-        self._student_storage.subscribe_in_discipline(self._aluno_id, materia_id)
-        self._student_storage.check_student_in_tree_disciplines(self._aluno_id)
+        self._aluno_id = self._storage.get_maximum_id()
+        self._storage.check_student_already_in_discipline(self._aluno_id, materia_id)
+        self._storage.subscribe_in_discipline(self._aluno_id, materia_id)
+        self._storage.check_student_in_tree_disciplines(self._aluno_id)
 
     def subscribe_in_course(self, curso_id):
-        aluno = self._student_storage.get_student(self._aluno_id)
+        self._storage.get_student(self._aluno_id)
         CourseController(self._conn).check_exists(curso_id)
-        self._student_storage.can_subscribe_course(self._aluno_id)
+        self._storage.can_subscribe_course(self._aluno_id)
+        self._storage.subscribe_in_course(self._aluno_id, curso_id)
